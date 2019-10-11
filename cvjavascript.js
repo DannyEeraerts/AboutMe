9babb1b83b055d969210a2e09847b5575f440604
danny <danny.eeraerts@proximus.be>
2019-10-11 11:36:05 +0200

    Fixed toggleclass

--- cvjavascript.js
+++ cvjavascript.js
@@ -1,25 +1,24 @@
-// window.onload=function() {
-//     const mapicon = document.querySelector(".fa-map-marker-alt");
-//     const frame = document.querySelector(".frame");
-//     console.log(frame);
-//     function showmap(){
-//         frame.classList.toggle('show');
-//         a = frame.classList.toggle('show');
-//         console.log(a);
-//     }
-//     mapicon.addEventListener("click", showmap);
-// }
+window.onload=function() {
+    const mapicon = document.querySelector(".fa-map-marker-alt");
+    console.log(mapicon);
+    const frame = document.querySelector(".map");
+    console.log(frame);
 
-(()=>{
-    const map = document.querySelector(".fa-map-marker-alt");
-    //event listener that will change the css class of the html object frame.
-    //create a variable with the object frame
-    //on click remove class and add class (toggle between classes)
-    map.addEventListener('click',function(){
-        const frame = document.querySelector(".map");
+    function showmap(){
+        //toggle class
         console.log(frame);
+        frame.classList.toggle('show');
         a = frame.classList.toggle('show');
         console.log(a);
-        frame.classList.toggle('show');
-    })
-})();
+        if (frame.classList.toggle('show')){
+            frame.classList.add('show');
+        }
+        else {
+            frame.classList.remove('show');
+        }
+        };
+
+        //event listener that will change the css class of the html object frame.    
+    
+        mapicon.addEventListener("click", showmap);
+ }
