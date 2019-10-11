2c9666e5d4934f27721622faf9a2b3a74ebb2b57
danny <danny.eeraerts@proximus.be>
2019-10-11 10:36:19 +0200

    rename javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-window.onload=function() {
-    const mapicon = document.querySelector(".fa-map-marker-alt");
-    
-    function showmap(){
-        mapicon.classList.toggle('show');
-    }
-
-    mapicon.addEventListener("click", showmap);
-}
