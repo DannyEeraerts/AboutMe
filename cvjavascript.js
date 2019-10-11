50b472c31850b3dd00bcfbaa739a91ccb6640457
danny <danny.eeraerts@proximus.be>
2019-10-11 11:16:41 +0200

    fix function

--- cvjavascript.js
+++ cvjavascript.js
@@ -15,9 +15,9 @@
     //event listener that will change the css class of the html object frame.
     //create a variable with the object frame
     //on click remove class and add class (toggle between classes)
-    map.addEventListener('click',function(e){
-        console.log(e.target);
+    map.addEventListener('click',function(){
         const frame = document.querySelector(".map");
+        console.log(frame);
         a = frame.classList.toggle('show');
         console.log(a);
         frame.classList.toggle('show');
