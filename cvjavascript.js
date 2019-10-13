6a0be1c5f1d79c72b599efa22d1246c97db307a8
Sander <sander@asocialmedia.be>
2019-10-13 15:15:34 +0200

    refactor toggleNode function

--- cvjavascript.js
+++ cvjavascript.js
@@ -3,8 +3,12 @@ window.onload=function() {
 const mapTriggerIcon = document.querySelector('.fa-map-marker-alt');
 const mapNode = document.querySelector('.map');
 
+function toggleNode(node, classname){
+    node.classList.toggle(classname);
+}
+
 mapTriggerIcon.addEventListener('click', function(){
-    mapNode.classList.toggle("show");
+    toggleNode(mapNode, "show");
 });
 
 }
