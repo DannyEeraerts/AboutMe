81bac4b2236672838dfaaa60278c5e0604b59a3e
Sander <sander@asocialmedia.be>
2019-10-13 14:29:08 +0200

    refactor

--- cvjavascript.js
+++ cvjavascript.js
@@ -1,20 +1,7 @@
-const mapButtonQuerySelector = ".fa-map-marker-alt";
-const mapQuerySelector=".map";
+window.onload=function() {
 
+document.querySelector('.fa-map-marker-alt').addEventListener('click', function(){
+    document.querySelector('.map').classList.toggle("show");
+});
 
-
-function toggleClassOnNode(selector, className) {
-    const item = document.querySelector(selector);
-    item.classList.toggle(className);
-  }
-
-
-// https://gomakethings.com/listening-for-click-events-with-vanilla-javascript/
-
-  document.addEventListener('click', function (event) {
-
-    if (event.target.matches(mapButtonQuerySelector)){
-
-        toggleClassOnNode(mapQuerySelector, "show");
-    } 
-}, false);
+}
